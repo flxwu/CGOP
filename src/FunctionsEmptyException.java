4a2059590521d92a461e8fbcf027536647b8b21c
David Wu <david10608@gmail.com>
2017-07-20 14:03:18 +0200

    Created FunctionsEmptyException extending NullPointerException for case there aren't any functions

---
+++ src/FunctionsEmptyException.java
@@ -0,0 +1,18 @@
+/**
+ * Project "CGOP"
+ * "FunctionsEmptyException.java" created by David Wu on Thu,20.07.17.
+ */
+
+public class FunctionsEmptyException extends NullPointerException {
+    public FunctionsEmptyException() {
+        super();
+    }
+
+    public FunctionsEmptyException(String msg) {
+        super(msg);
+    }
+}
+
+/**
+ * End of FunctionsEmptyException.java
+ */
